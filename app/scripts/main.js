697ab02192578a6fd27326f8eca880d9e8d3eb02
cegn <cegn@senado.gov.br>
2015-12-16 18:23:09 -0200

    prototipo finalizado

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,20 @@
 ;!function () {
 
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
+
+  var pops =   $(".mapa-svg").popover({
+    'container': 'body',
+    'placement': 'top',
+    'trigger': 'click hover',
+    'delay' : {
+      "show" : 300,
+      "hide" : 200
+    }
+  }); 
+  pops.on('click',function(){
+    pops.not(this).popover('hide');
+  }).on('mouseover',function(){
+    pops.not(this).popover('hide');
+  })
 
 }()
