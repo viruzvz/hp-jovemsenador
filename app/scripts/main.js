411cb2b54740261850436beb20e46d9f7373e3f6
cegn <cegn@senado.gov.br>
2015-12-18 10:50:01 -0200

    prototipo finalizado

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,7 @@
     'container': 'body',
     'placement': 'top',
     'trigger': 'click hover',
+    'html': true,
     'delay' : {
       "show" : 300,
       "hide" : 200
@@ -17,4 +18,6 @@
     pops.not(this).popover('hide');
   })
 
+
+  $('[data-original-title="Maranhão"]').attr('data-content','<strong>Escolas Participantes:</strong> 485<br><strong>Coordenadores:</strong> Cláudia Ribeiro e Telma Regina Oliveira Peres<br><strong>E-mail:</strong> regina@gov.br, telma@gov.br<br><strong>Secretaria de Educação do Estado de Minas Gerais</strong><br>Rod. Prefeito Américo Gianetti, S/N - Ed. Minas, 11º Andar<br><span>Lado par / Superintendência de Ensino Médio - SEM<br></span><span>Cidade Administrativa Presidente Tancredo Neves<br></span><span>Bairro Serra Verde<br></span><span>Belo Horizonte – MG<br></span><span>CEP: 31.630-901</span></span>');
 }()
