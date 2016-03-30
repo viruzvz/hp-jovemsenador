506d553999839ce4cd42fa82acb4b3616aa56b67
Carlos Eduardo Gomes Neri <cegn@senado.leg.br>
2016-03-30 20:38:31 -0300

    atualizações pgwslideshow, nanoscroller, ajustes na agenda, add rodape entre outros

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,5 @@
 ;!function () {
 
-  $('[data-toggle="tooltip"]').tooltip();
-
   var pops =   $(".mapa-svg").popover({
     'container': 'body',
     'placement': 'top',
@@ -20,7 +18,14 @@
 
   $('[data-original-title="Maranhão"]').attr('data-content','<img src="../img/flags/MA_p.gif"/><br><strong>Escolas Participantes:</strong> 485<br><strong>Coordenadores:</strong> Cláudia Ribeiro e Telma Regina Oliveira Peres<br><strong>E-mail:</strong> regina@gov.br, telma@gov.br<br><strong>Secretaria de Educação do Estado de Minas Gerais</strong><br>Rod. Prefeito Américo Gianetti, S/N - Ed. Minas, 11º Andar<br><span>Lado par / Superintendência de Ensino Médio - SEM<br></span><span>Cidade Administrativa Presidente Tancredo Neves<br></span><span>Bairro Serra Verde<br></span><span>Belo Horizonte – MG<br></span><span>CEP: 31.630-901</span></span>');
 
-}()
+
+
+  $(document).ready(function() {
+      $('[data-toggle="tooltip"]').tooltip();
+      $('.pgwSlideshow').pgwSlideshow();
+      $('.nano').nanoScroller();
+  });
+
 function toggleChevron(e) {
     $(e.target)
         .prev('.panel-heading')
@@ -29,3 +34,6 @@ function toggleChevron(e) {
 }
 $('#accordion').on('hidden.bs.collapse', toggleChevron);
 $('#accordion').on('shown.bs.collapse', toggleChevron);
+
+
+}()
